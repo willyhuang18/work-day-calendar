d336cda6138c9edf85969771809e27402d00d740
wiily <willyhuang18@gmail.com>
2022-01-06 00:26:23 -0800

    setting begining of the day

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //using moment.js and jQuery to indicate current time
 var now = moment();
+//making the text to disolay in the website
 $("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
+// set to 12:00 am today
+var currentTime = moment().startOf('d').add(10, 'h');
+console.log(currentTime);
+var currentHour = moment().format('H');
+console.log(currentHour);
