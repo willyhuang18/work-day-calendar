6d4de7a993f11d956b744847f72a07722b0c477c
wiily <willyhuang18@gmail.com>
2022-01-06 15:57:48 -0800

    styling the content

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ var now = moment();
 //making the text to disolay in the website
 $("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
 // set to 12:00 am today
-var currentTime = moment().startOf('d').add(10, 'h');
+var currentTime = moment().startOf('day').add(3, 'hour');
 console.log(currentTime);
 var currentHour = moment().format('H');
 console.log(currentHour);
 
 //making the timeblocks for the day
-for (var i = 10; i < 24; i++) {
+for (var i = 8; i < 24; i++) {
     var timeSlot = currentTime.add(1, "hour").format("HH:mm A");
     //give a temp variable for indicate future use
-    var temp;
-}
 //if statement for timeblocks
-console.log(timeSlot);
