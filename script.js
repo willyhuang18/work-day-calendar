334f7580d810b9adffde7411afb2b0dc313e1013
wiily <willyhuang18@gmail.com>
2022-01-06 16:06:16 -0800

    giving if statement for color

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ var currentTime = moment().startOf('day').add(3, 'hour');
 console.log(currentTime);
 var currentHour = moment().format('H');
 console.log(currentHour);
+//declare state variable for changing color
+var state = document.querySelector(".state")
 
 //making the timeblocks for the day
 for (var i = 8; i < 24; i++) {
     var timeSlot = currentTime.add(1, "hour").format("HH:mm A");
     //give a temp variable for indicate future use
 //if statement for timeblocks
+    if(currentHour == i){
+        state.classList.add("activePresent")
+    }else if(currentHour > i){
+        state.classList.add("activeFuture")
+    }else if(currentHour < i){
+        state.classList.add("activePast")
+    }
+}
